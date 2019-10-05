0a9ef6f957d0289bc763359f2527bc53d20947eb
LGA1150 <dqfext@gmail.com>
2019-10-05 22:16:02 +0800

    package: import lean packages

--- scripts/download.pl
+++ scripts/download.pl
@@ -82,7 +82,7 @@ sub download_cmd($) {
 	}
 
 	return $have_curl
-		? (qw(curl -f --connect-timeout 20 --retry 5 --location --insecure), shellwords($ENV{CURL_OPTIONS} || ''), $url)
+		? (qw(curl --http1.1 -f --connect-timeout 20 --retry 5 --location --insecure), shellwords($ENV{CURL_OPTIONS} || ''), $url)
 		: (qw(wget --tries=5 --timeout=20 --no-check-certificate --output-document=-), shellwords($ENV{WGET_OPTIONS} || ''), $url)
 	;
 }
